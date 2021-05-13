be9c4053cbf3409bc9da199323ccdf92fc0b7cb7
Abhinav-26 <abhinavdubey616@gmail.com>
2021-05-13 13:29:33 +0530

    basic-listing-in-views

--- Contact-App/reactnd-contacts-app/src/App.js
+++ Contact-App/reactnd-contacts-app/src/App.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import ListContacts from './ListContacts'
+
+const contacts = [
+  {
+    "id": "karen",
+    "name": "Karen Isgrigg",
+    "handle": "karen_isgrigg",
+    "avatarURL": "http://localhost:5001/karen.jpg"
+  },
+  {
+    "id": "richard",
+    "name": "Richard Kalehoff",
+    "handle": "richardkalehoff",
+    "avatarURL": "http://localhost:5001/richard.jpg"
+  },
+  {
+    "id": "tyler",
+    "name": "Tyler McGinnis",
+    "handle": "tylermcginnis",
+    "avatarURL": "http://localhost:5001/tyler.jpg"
+  }
+ ];
 
 class App extends Component {
   render() {
     return (
       <div>
-        Hello World
+        <ListContacts contacts={contacts} />
       </div>
     );
   }
